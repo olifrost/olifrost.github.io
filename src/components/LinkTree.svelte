0c8e94d1546c90d911c778ecf9bb44bb11edd278
Oli Frost <mail@olifro.st>
2025-03-29 09:01:03 +0000

    Added social links to link tree

--- src/components/LinkTree.svelte
+++ src/components/LinkTree.svelte
@@ -3,21 +3,15 @@
   import { links, streamingLinks } from '../data/links';
 </script>
 
-<div class="min-h-screen flex items-center">
-  <div class="max-w-2xl mx-auto px-4 py-8 w-full">
-    <h1 class="text-3xl font-bold mb-4 text-center">Oli Frost</h1>
-    
-    <nav class="space-y-2 w-full">
-      {#each links.docs as link}
-        <LinkItem 
-          title={link.title}
-          url={link.url}
-          icon={link.icon}
-          color={link.colour}
-          isExpandable={link.isExpandable}
-          children={link.children}
-        />
-      {/each}
-    </nav>
-  </div>
-</div>
+<nav class="space-y-2 w-full">
+  {#each links.docs as link}
+    <LinkItem 
+      title={link.title}
+      url={link.url}
+      icon={link.icon}
+      color={link.colour}
+      isExpandable={link.isExpandable}
+      children={link.children}
+    />
+  {/each}
+</nav>
